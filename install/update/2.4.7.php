4c8a27861c5f3bd4a2b484da6d0af9c9f9eef9ff
sarakha63 <sarakha_ludovic@yahoo.fr>
2017-06-28 19:35:25 +0200

    Update 2.4.7.php

--- install/update/2.4.7.php
+++ install/update/2.4.7.php
@@ -1,7 +1,7 @@
 <?php
 echo "Preparing .....\n";
 shell_exec('sudo mkdir -p /home/jeedomtmp');
-shell_exec('sudo mv /tmp /home/jeedomtmp');
+shell_exec('sudo mv /tmp/* /home/jeedomtmp');
 jeedom::update();
 die();
 ?>
