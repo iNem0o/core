8c6c512f9e330354260702312915be857feb47cd
sarakha63 <sarakha63@DESKTOP-D9IGIEV>
2017-06-29 15:05:44 +0200

    update

--- install/update/2.4.7.php
+++ install/update/2.4.7.php
@@ -2,6 +2,16 @@
 echo "Preparing .....\n";
 shell_exec('sudo mkdir -p /home/jeedomtmp');
 shell_exec('sudo mv /tmp/* /home/jeedomtmp');
+echo "OK\n";
+echo 'Relance de la mise à jour (normal). A partir de là il faut encore attendre 5 minutes.\n';
+echo 'La log peut etre illisible pour suivre l\'avancement allez dans log puis choississez update.\n';
+echo 'Relance de la mise à jour (normal). A partir de là il faut encore attendre 5 minutes.\n';
+echo 'La log peut etre illisible pour suivre l\'avancement allez dans log puis choississez update.\n';
+echo 'Relance de la mise à jour (normal). A partir de là il faut encore attendre 5 minutes.\n';
+echo 'La log peut etre illisible pour suivre l\'avancement allez dans log puis choississez update.\n';
+echo 'Relance de la mise à jour (normal). A partir de là il faut encore attendre 5 minutes.\n';
+echo 'La log peut etre illisible pour suivre l\'avancement allez dans log puis choississez update.\n';
+sleep(20);
 jeedom::update();
 die();
 ?>
