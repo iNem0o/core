1fb66fdeb07019d19adf1249a37b0ba00e572419
sarakha63 <sarakha63@DESKTOP-D9IGIEV>
2017-06-29 16:06:03 +0200

    mask some actions

--- install/update/2.4.7.php
+++ install/update/2.4.7.php
@@ -1,8 +1,8 @@
 <?php
 echo "Preparing .....\n";
-shell_exec('sudo mkdir -p /home/jeedomtmp');
-shell_exec('sudo mv /tmp/* /home/jeedomtmp');
-shell_exec('sudo rm -rf /tmp/jeedom*');
+shell_exec('sudo mkdir -p /home/jeedomtmp >/dev/null 2>&1');
+shell_exec('sudo mv /tmp/* /home/jeedomtmp >/dev/null 2>&1');
+shell_exec('sudo rm -rf /tmp/jeedom* >/dev/null 2>&1');
 echo "OK\n";
 echo 'Relance de la mise à jour (normal). A partir de là il faut encore attendre 5 minutes.\n';
 echo 'La log peut etre illisible pour suivre l\'avancement allez dans log puis choississez update.\n';
